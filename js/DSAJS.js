97a166c4a87a424ec564120e78f73ababe8c0112
sambee coding <samrisamrawit30@@email.com>
2025-08-23 00:36:15 -0700

    auto-commit

--- js/DSAJS.js
+++ js/DSAJS.js
@@ -14,3 +14,30 @@ const store=["underarmbag","toutebag","bacpak"];
      return arr[index];
   }
   console.log(getElement(numbers,0));
+
+
+
+  let me=["pen","lens","glass"];
+  function myRandomItem(array){
+ let item=array;
+for(let i=0;i<item.length;i++){
+   
+    console.log(item[i])
+}
+  }
+  myRandomItem(me);
+
+  
+  let you=["pen","lens","glass"];
+  function myRandomItem(array){
+
+for(let i=0;i<array.length;i++){
+   let us=Math.random(array);
+   console.log(us);
+   
+}
+  }
+  myRandomItem(me);
+
+
+  
