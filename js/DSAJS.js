b3200edd12e2aa4ff43ddf0aace272f04effacd2
sambee coding <samrisamrawit30@@email.com>
2025-08-24 14:54:20 -0700

    auto-commit

--- js/DSAJS.js
+++ js/DSAJS.js
@@ -17,3 +17,16 @@ clickingBottle.addEventListener("click", flipBottle);
 
 // finding small letter from the given scribbled letters
    
+function addThem(nums,target){
+for(let i=0;i<nums.length;i++){
+    for(let j=i+1;j<nums.length;j++){
+        if(nums[i] + nums[j]===target){
+            console.log([i,j]);
+        }
+    }
+}
+}
+  
+let nums=[2,7,11,15]
+let target=9;
+addThem(nums,target);
